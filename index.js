97fde40aa01141817fcc9f1c5f570d44474359f2
Soumalya Ghosh <86161882+ServerDeveloper9447@users.noreply.github.com>
2022-01-02 18:47:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const app = require('express')()
 app.set('json_spaces', 1)
 app.use(require('cors')())
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
   if (!req.query.url) {
     res.send({'error':"No url provided"})
   } else {
     try {
-  var time = Date.now()
+      var time = Date.now()
   const fetch = require('node-fetch')
   const f = await fetch(req.query.url)
+  const st = await f.status
   const json = await f.json()
-  const st = {status: await f.status}
-  res.send(Object.assign(json, st, {lag: Math.floor(Date.now() - time)}))
-  } catch(err) {
-    res.send({error:"The URL doesn't provide a valid JSON response or is protecting itself from getting requests from automated clients."})
-  }
+  res.send([json, {"status":st,lag:Math.floor(Date.now() - time}])
+    } catch (err) {
+      res.send({error:"The URL doesn't provide a valid JSON response or is protecting itself from getting requests from automated clients."})
+    }
   }
 })
 
 app.listen(process.env.PORT, () => {
   console.log('running')
-})
+}) 
