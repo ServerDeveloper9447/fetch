f86a08aeb646cb74142fd35861c2edce5127a410
Soumalya Ghosh <86161882+ServerDeveloper9447@users.noreply.github.com>
2022-01-03 17:51:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('express')()
-
+const fetch = require('node-fetch')
 app.set('json_spaces', 1)
 app.use(require('cors')())
 
@@ -9,13 +9,15 @@ app.get('/', async (req, res) => {
   } else {
     try {
       var time = Date.now()
-  const fetch = require('node-fetch')
   const f = await fetch(req.query.url)
   const st = await f.status
   const json = await f.json()
   res.send([json, {"status":st,lag:Math.floor(Date.now() - time)}])
     } catch (err) {
-      res.send([{error:"The URL doesn't provide a valid JSON response or is protecting itself from getting requests from automated clients."}])
+      var time1 = Date.now()
+      const f1 = await fetch(req.query.url)
+      const st1 = await f1.status
+      res.send([{error:"The URL doesn't provide a valid JSON response or is protecting itself from getting requests from automated clients."}, {"status":st1,lag:Math.floor(Date.now() - time1)}])
     }
   }
 })
